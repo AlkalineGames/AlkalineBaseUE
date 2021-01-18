149b7177c81c357eb2ab7b61eef803a984835d14
Christopher Augustus <repos@c4augustus.com>
2021-01-18 20:28:29 +0000

    create AlkUemChar module implementation under Private

---
+++ Source/AlkUemChar/Private/AlkUemChar.cpp
@@ -0,0 +1,10 @@
+// Copyright 2021 Alkaline Games, LLC.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+#include "AlkUemChar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_GAME_MODULE(FDefaultGameModuleImpl, AlkUemChar);
