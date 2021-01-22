749f7fcdcc084f066ad7118cedb9f9e418029a8b
Christopher Augustus <repos@c4augustus.com>
2021-01-22 14:06:41 +0000

    create Source module AlkUemPure and its module implementation under Private, intended to provide only FP-style pure functions, i.e. no mutation or side effects

---
+++ Source/AlkUemPure/AlkUemPure.Build.cs
@@ -0,0 +1,22 @@
+// Copyright 2021 Alkaline Games, LLC.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+using UnrealBuildTool;
+
+public class AlkUemPure : ModuleRules
+{
+  public AlkUemPure(ReadOnlyTargetRules Target) : base(Target)
+  {
+    bLegacyPublicIncludePaths = false;
+      // ^ !!! fixes VC error: command line is too long to fit in debug record
+    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    PrivateDependencyModuleNames.AddRange(new string[] {
+      "Core",
+      "CoreUObject",
+      "Engine"
+    });
+  }
+}
