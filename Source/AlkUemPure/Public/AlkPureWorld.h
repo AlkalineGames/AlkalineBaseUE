31cecf63225954a24b9153acef50abb5dc164413
Christopher Augustus <repos@c4augustus.com>
2021-01-23 15:35:22 +0000

    create AlkPureWorld.h function inline const FVector2D pure::WorldGameViewportSize(const UWorld*)

--- Source/AlkUemPure/Public/AlkPureWorld.h
+++ Source/AlkUemPure/Public/AlkPureWorld.h
@@ -4,12 +4,22 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
+#include "Engine/GameViewportClient.h"
 #include "Engine/World.h"
 
 namespace pure {
 
 inline const float WorldRealTimeSeconds(const UWorld* world) {
   return world ? world->GetRealTimeSeconds() : 0.f;
+}
+
+inline const FVector2D WorldGameViewportSize(const UWorld* world) {
+  FVector2D result;
+  if (world) {
+    auto viewport = world->GetGameViewport();
+    if (viewport) viewport->GetViewportSize(result);
+  }
+  return result;
 }
 
 }; // end namespace pure
