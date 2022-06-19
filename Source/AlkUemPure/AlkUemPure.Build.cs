bf65c8ecc07cdf64fc73d47c00cf6ec87a880817
Christopher Augustus <repos@c4augustus.com>
2022-06-19 21:41:16 +0100

    enhance AlkUemPure to ensure that Android targets use the proper version of the SDK build tools

--- Source/AlkUemPure/AlkUemPure.Build.cs
+++ Source/AlkUemPure/AlkUemPure.Build.cs
@@ -18,5 +18,12 @@ public class AlkUemPure : ModuleRules
       "CoreUObject",
       "Engine"
     });
+
+    if (Target.Platform == UnrealTargetPlatform.Android)
+    {
+      // required customizations for the Android build process
+      string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+      AdditionalPropertiesForReceipt.Add("AndroidPlugin", System.IO.Path.Combine(PluginPath, "AlkUemPure_APL.xml"));
+    }
   }
 }
